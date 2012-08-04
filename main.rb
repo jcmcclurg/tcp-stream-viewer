15e4dbf0fe53085c2fdb862a4922258c3a1b992a
jcmcclurg <jcmcclurg@gmail.com>
2012-08-04 00:59:44 -0500

    still testing

--- main.rb
+++ main.rb
@@ -1,70 +1,83 @@
 require 'rubygems'
 require 'sinatra'
 
-class Stream
-   attr_accessor :client, :server, :packets, :transactions
-
-   def initialize(client, server, transactions)
-      @client = client
-      @server = server
-      @transactions = transactions
-   end
-end
-
-class Packet
-   attr_accessor :data, :direction
-
-   def initialize(data, direction)
-      @data = data
-      @direction = direction
-   end
-end
-
 class Transaction
-   attr_accessor :clientRequest, :serverResponse
-
-   def initialize(clientRequest, serverResponse)
-      @clientRequest = clientRequest
+   attr_reader :clientRequest, :serverResponse, :clientRequestURI
+   
+   def initialize()
+     @serverResponse = "";
+     @clientRequest  = ""; 
+   end
+   
+   def serverResponse=(response)
       @serverResponse = serverResponse
+      @serverResponseStatus = getResponseStatus(serverResponse)
+   end
+   
+   def clientRequest=(response)
+      @clientRequest = clientRequest
+      @clientRequestURI = getRequestURI(clientRequest)
+   end
+
+   private
+
+   def getRequestURI(request)
+      match = request.match(/\AGET ([^\n\r ]+) .+Host: ([^\n\r]+)/m)
+      
+      if !match.nil?
+         match[2].concat(match[1])
+      end
+   end
+
+   def getResponseStatus(response)
+      match = request.match(/\AHTTP[^\r\n ] ([0-9]+ [^\r\n]+)/m)
+      
+      if !match.nil?
+         match[1]
+      end
    end
 end
 
+# This Middleware application serves up the requested captured HTTP response.
 class MyMiddleware
    def initialize(app)
       @app = app
    end
 
    def call(env)
-      status, headers, body = @app.call(env)
-
       if status == 999
          puts "crayzay"
-         
-         serverResponse = Rack::serverResponse.new
-         serverResponse.body = Streams["3"].transactions[0].serverResponse
-         serverResponse.finish
+
+         rack_input = Streams["3"].transactions[0].serverResponse
+         env.update({"rack.input"=> rack_input})
       else
          puts "normal #{status}"
-         
-         [status,headers,body]
       end
+      status, headers, body = @app.call(env)
+      [status,headers,body]
    end
 end
 
+# Use wireshark to do initial parsing of the raw dump file. Our goal is to
+# eventually mimic the "Follow Stream" behavior of the wireshark dialog box.
 dumpFile = "testing/josiahland.wireshark.tcpdump"
 parsedPackets = `tshark -r #{dumpFile} -R "tcp.stream && data" -T fields -e tcp.stream -e ip.src -e tcp.srcport -e ip.dst -e tcp.dstport -e data`
 parsedPackets = parsedPackets.split("\n")
-Streams = Hash.new
 
-# parse packets into Streams
+# We now have an array of individual packets. Let's separate these packets out
+# into arrays of individual streams. The format of this array will be:
+# "clientIP:port serverIP:port" => array of Transaction objects
+Streams = Hash.new # The stream array
+transactionMode = Hash.new # A temporary variable to keep track of the
+# current stream directions
+
 parsedPackets.each do |parsedPacket|
    parsedPacket = parsedPacket.split("\t")
 
-   streamID = parsedPacket[0]
-   source = "#{parsedPacket[1]}:#{parsedPacket[2]}"
-   dest   = "#{parsedPacket[3]}:#{parsedPacket[4]}"
+   streamID = "#{parsedPacket[0]} #{parsedPacket[1]}:#{parsedPacket[2]} #{parsedPacket[3]}:#{parsedPacket[4]}"
 
-
+   # Wireshark displays the packet payload as an ASCII hexadecimal digit. We're
+   # going to change this back into a UTF-8 string
    packetBytes = Array.new
    packetData = parsedPacket[5].scan(/.{2}/)
    packetData.each do |chr|
@@ -72,66 +85,56 @@ parsedPackets.each do |parsedPacket|
    end
    packetData = packetBytes.pack("U*")
 
-   if !packetData.match(/\AGET [^\n]+ HTTP/).nil?
-      if !Streams.has_key?(streamID)
-         Streams[streamID] = Stream.new(source, dest, Array.new, Array.new)
+   # Switch stream directions on GET requests and HTTP responses
+   if !packetData.match(/\AGET [^\r\n ]+/).nil?
+      if transactionMode[streamID] == "clientRequest"
+         abort("Got confused by two GET requests in a row.")
       end
+
+      transactionMode[streamID] = "clientRequest"
+      
+      if !Streams.has_key?(streamID)
+         Streams[streamID] = Array.new
+      end
+      Streams[streamID].push(Transaction.new)
+   
+   elsif transactionMode[streamID] == "clientRequest" && !packetData.match(/\AHTTP/).nil?
+      trasactionMode[streamID] = "serverResponse"
+      
+   end # Switch stream directions
+
+   if !Streams.has_key?(streamID)
+      abort("Got confused by a stream with no GET request")
+   end
+   
+   if trasactionMode[streamID] == "clientRequest"
+      Streams[streamID].last.clientRequest += packetData
+   else
+      Streams[streamID].last.serverResponse += packetData
    end
 
-   if Streams.has_key?(streamID)
-      direction = ">"
-      if source == Streams[streamID].server
-         direction = "<"
-      end
+end# Parse packets into Streams
 
-      if Streams[streamID].packets.size == 0 || (direction == ">" && Streams[streamID].packets.last.direction == "<")
-         match = packetData.match(/\AGET ([^\r\n]+) HTTP.+\r?\nHost: ([^\r\n]+)/m)
-         Streams[streamID].transactions.push(transaction.new(match[2].concat(match[1]),"",""))
-      end
-
-      if direction == ">"
-         Streams[streamID].transactions.last.fullclientRequest.concat(packetData)
-      else
-         Streams[streamID].transactions.last.serverResponse.concat(packetData)
-      end
-
-      Streams[streamID].packets.push(Packet.new(packetData, direction))
-   end # if
-
-end # parse packets into Streams
-
-
-# parse Streams into serverResponse pairs
-serverResponses = Array.new
-Streams.each_value do |stream|
-   
-end # parse Streams into serverResponse pairs
-
+# Start piping everything through the custom Middleware application we've built
+# to serve up the captured HTTP responses
 use MyMiddleware
 
 get "/" do
-   # print Streams
+# print Streams
    outputStr = String.new
-   Streams.each_pair do |streamID, stream|      
-      outputStr << "############ (#{streamID}) #{stream.client} -> #{stream.server} #############\n(#{stream.packets.size} packets, #{stream.transactions.size} serverResponse pairs)\n"
+   Streams.each_pair do |streamID, stream|
+      outputStr << "<h1>#{streamID}</h1>\n"
       outputStr << "<ul>\n"
-      counter = 0
-      stream.packets.each do |packet|
-        outputStr << "<li><code>#{packet.data.encode({:xml => :text})}</code></li>"
-        counter += 1
-      end
-=begin
+
       stream.transactions.each do |transaction|
-         outputStr << "<li><a href=\"#{transaction.clientRequest}\" title=\"#{transaction.fullclientRequest}\">#{transaction.clientRequest}</a></li>\n"
-         counter += 1
+         outputStr << "<li><a href=\"#{transaction.clientRequestURI}\" title=\"#{transaction.clientRequest}\">#{transaction.clientRequestURI}</a></li>\n"
       end
-=end
+
       outputStr << "</ul>\n"
       outputStr << "\n\n"
-   end # print Streams
+   end# print Streams
 
-
-   "<html><header><title>Hi</title></header><body>Hello World! You are a good world.\n\n#{outputStr}</body></html>"
+   "<html><header><title>Hi</title></header><body>Hello World! You are a good world.\n<br />\n#{outputStr}</body></html>"
 end
 
 get %r{/bubba} do
